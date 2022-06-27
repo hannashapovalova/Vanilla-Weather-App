96fc088135f0ba96288ec94a12c460bb9e6e5b2e
hannashapovalova <hannalova1991@gmail.com>
2022-06-27 20:07:34 +0100

    Add current time JS

---
+++ src/data.js
@@ -0,0 +1,53 @@
+// Current Dates, Hours, Minutes, Day, Months
+
+let now = new Date ();
+let date = document.querySelector(".current-date");
+let currentDate = now.getDate();
+date.innerHTML = `${currentDate}`;
+
+let housrs = document.querySelector(".current-hours");
+let currentHours = now.getHours();
+if (currentHours < 10) {
+    currentHours = `0${currentHours}`;
+} 
+housrs.innerHTML = `${currentHours}`;
+
+
+
+let minutes = document.querySelector(".current-minutes");
+let currentMinutes = now.getMinutes();
+if (currentMinutes < 10) {
+    currentMinutes = `0${currentMinutes}`;
+}
+minutes.innerHTML = `${currentMinutes}`;
+
+let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+  let currentMonth = months[now.getMonth()];
+  let month = document.querySelector(".current-month");
+  month.innerHTML = `${currentMonth}`;
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+]
+let currentDayOfWeeek = days[now.getDay()];
+let dayOfWeek = document.querySelector(".current-day-of-week");
+dayOfWeek.innerHTML = `${currentDayOfWeeek}`;
